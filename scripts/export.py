24e5eb64ef6329b89491f853df540264596dbc06
Artem Chibisov <artchibisov10@gmail.com>
2026-02-20 23:52:46 +0400

    redis for prod

--- scripts/export.py
+++ scripts/export.py
@@ -2,10 +2,10 @@ import redis
 import json
 
 r = redis.Redis(
-    host="redis-12487.c278.us-east-1-4.ec2.cloud.redislabs.com",
-    port=12487,
+    host="redis-18396.c13.us-east-1-3.ec2.cloud.redislabs.com",
+    port=18396,
     username="default",
-    password="VfW7UIF9fCqWfgEB4BisP0qGBCYMx5QA",
+    password="o0vbOITUgcYpHP7vSrictJvCjKnSuBS3",
     decode_responses=True
 )
 
@@ -17,7 +17,7 @@ for key in r.scan_iter("*"):
     except:
         data[key] = "NON-STRING"
 
-with open("dump.json", "w") as f:
+with open("dump2.json", "w") as f:
     json.dump(data, f, indent=2)
 
 print("Export complete")
